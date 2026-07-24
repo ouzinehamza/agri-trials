e7fb2d1a8acb069be898c873d6d0b841d1c61087
Ouzine Hamza <hamzaouzine@hotmail.com>
2026-07-24 05:35:08 +0100

    disable register action

--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -111,7 +111,7 @@ export default function Register() {
                         Already registered?
                     </Link>
 
-                    <PrimaryButton className="ms-4" disabled={processing}>
+                    <PrimaryButton className="ms-4" disabled>
                         Register
                     </PrimaryButton>
                 </div>
